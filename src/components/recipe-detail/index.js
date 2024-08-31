495439c3a07f25ddb5e19a599d8deb1079000ca0
tharuAnish <namoanishtharu@gmail.com>
2024-08-31 19:16:33 +0530

    improved rp detail page

--- src/components/recipe-detail/index.js
+++ src/components/recipe-detail/index.js
@@ -1,93 +1,95 @@
+import BackBtn from "../back-btn"
+import { CardTitle } from "../ui/card"
+
 export default function RecipeDetailsPage({ recipeDetail }) {
   return (
-    <div className="max-w-4xl mx-auto p-8 font-sans text-gray-800 bg-gray-50 min-h-screen">
-      <div className="flex items-center mb-8 p-4 bg-white rounded-lg shadow-md">
+    <div className="max-w-6xl mx-auto p-8 font-sans text-gray-800  min-h-screen">
+      <div className="flex justify-between">
+        <CardTitle className="mb-9">Recipes</CardTitle> <BackBtn />
+      </div>
+      <div className="flex mb-8 p-4 bg-stone-50 rounded-lg shadow-md">
         <img
           src={recipeDetail.image}
           alt={recipeDetail.name}
-          className="w-36 h-36 object-cover rounded-md shadow-lg"
+          className="w-60 basis-2/5 object-cover rounded-md shadow-lg"
         />
-        <h1 className="text-4xl font-bold ml-6">{recipeDetail.name}</h1>
-      </div>
+        <div className="ml-6 basis-3/5 ">
+          <h1 className="text-4xl mt-5 font-bold ">{recipeDetail.name}</h1>
 
-      <div className="mb-8 bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold mb-4 border-b pb-2">
-          Ingredients
-        </h2>
-        <ul className="list-disc pl-6 space-y-2">
-          {recipeDetail.ingredients.map((ingredient, index) => (
-            <li key={index} className="text-lg">
-              {ingredient}
-            </li>
-          ))}
-        </ul>
-      </div>
+          <div className="flex gap-3 mt-2 text-sm">
+            <p>
+              <strong className="font-medium ">Rating:</strong>{" "}
+              {recipeDetail.rating} (based on {recipeDetail.reviewCount}{" "}
+              reviews)
+            </p>
+            <p>
+              <strong className="font-medium">Meal Type:</strong>{" "}
+              {recipeDetail.mealType.join(", ")}
+            </p>
+          </div>
 
-      <div className="mb-8 bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold mb-4 border-b pb-2">
-          Instructions
-        </h2>
-        <ol className="list-decimal pl-6 space-y-2">
-          {recipeDetail.instructions.map((instruction, index) => (
-            <li key={index} className="text-lg">
-              {instruction}
-            </li>
-          ))}
-        </ol>
-      </div>
+          <div className="mt-3 mb-1">
+            {recipeDetail.tags.map((tag, index) => (
+              <span
+                key={index}
+                className="inline-block bg-gray-50 border text-gray-700 rounded-full px-5 py-1 text-sm  mr-2 mb-2 transition-colors hover:bg-gray-300"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
 
-      <div className="mb-8 bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold mb-4 border-b pb-2">Details</h2>
-        <div className="space-y-2">
-          <p>
-            <strong className="font-medium">Prep Time:</strong>{" "}
-            {recipeDetail.prepTimeMinutes} minutes
-          </p>
-          <p>
-            <strong className="font-medium">Cook Time:</strong>{" "}
-            {recipeDetail.cookTimeMinutes} minutes
-          </p>
-          <p>
-            <strong className="font-medium">Servings:</strong>{" "}
-            {recipeDetail.servings}
-          </p>
-          <p>
-            <strong className="font-medium">Difficulty:</strong>{" "}
-            {recipeDetail.difficulty}
-          </p>
-          <p>
-            <strong className="font-medium">Cuisine:</strong>{" "}
-            {recipeDetail.cuisine}
-          </p>
-          <p>
-            <strong className="font-medium">Calories per Serving:</strong>{" "}
-            {recipeDetail.caloriesPerServing}
-          </p>
-          <div className="mt-2">
-            <strong className="font-medium">Tags:</strong>
-            <div className="mt-1">
-              {recipeDetail.tags.map((tag, index) => (
-                <span
-                  key={index}
-                  className="inline-block bg-gray-200 text-gray-700 rounded-full px-4 py-1 text-sm font-semibold mr-2 mb-2 transition-colors hover:bg-gray-300"
-                >
-                  {tag}
-                </span>
+          <div className="gap-2  flex flex-wrap">
+            <p>
+              <strong className="font-medium">Prep Time:</strong>{" "}
+              {recipeDetail.prepTimeMinutes} minutes,
+            </p>
+            <p>
+              <strong className="font-medium">Cook Time:</strong>{" "}
+              {recipeDetail.cookTimeMinutes} minutes,
+            </p>
+            <p>
+              <strong className="font-medium">Servings:</strong>{" "}
+              {recipeDetail.servings},
+            </p>
+            <p>
+              <strong className="font-medium">Difficulty:</strong>{" "}
+              {recipeDetail.difficulty},
+            </p>
+            <p>
+              <strong className="font-medium">Cuisine:</strong>{" "}
+              {recipeDetail.cuisine},
+            </p>
+            <p>
+              <strong className="font-medium">Calories per Serving:</strong>{" "}
+              {recipeDetail.caloriesPerServing},
+            </p>
+          </div>
+
+          <div className="">
+            <h2 className="text-xl font-semibold mb-2 mt-4 w-1/3 border-b pb-2">
+              Ingredients
+            </h2>
+            <ul className="list-disc grid gap-x-5 grid-cols-2 pl-6 ">
+              {recipeDetail.ingredients.map((ingredient, index) => (
+                <li key={index} className="">
+                  {ingredient}
+                </li>
               ))}
-            </div>
+            </ul>
           </div>
         </div>
       </div>
 
-      <div className="text-gray-700 bg-white p-6 rounded-lg shadow-md">
-        <p className="mb-2">
-          <strong className="font-medium">Rating:</strong> {recipeDetail.rating}{" "}
-          (based on {recipeDetail.reviewCount} reviews)
-        </p>
-        <p>
-          <strong className="font-medium">Meal Type:</strong>{" "}
-          {recipeDetail.mealType.join(", ")}
-        </p>
+      <div className="mb-8  pt-4">
+        <h2 className="text-xl font-semibold mb-4 border-b pb-2">
+          Instructions
+        </h2>
+        <ol className="list-decimal pl-6 space-y-2">
+          {recipeDetail.instructions.map((instruction, index) => (
+            <li key={index}>{instruction}</li>
+          ))}
+        </ol>
       </div>
     </div>
   )
